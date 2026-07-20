1dcea59c7f9ece830594e315a576770e7c46abe9
CayllahuaPedro <65582385+CayllahuaPedro@users.noreply.github.com>
2026-07-20 11:38:15 -0300

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/search-for-word/submission-20.py
@@ -0,0 +1,30 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def recuExist(i: int, j:int, k: int) -> bool:
+            if i > len(board) -1 or i < 0:
+                return False
+            if j > len(board[0]) -1 or j < 0:
+                return False
+            if (i, j) in visited:
+                return False
+            char = board[i][j]
+            if k == len(word)-1 and word[k] == char:
+                return True
+            count = k
+            if char != word[k]:
+                return False
+            else:
+                count = k+1
+            visited.add((i,j))
+            res = (recuExist(i+1, j, count) or recuExist(i, j+1, count) 
+            or recuExist(i-1, j, count) or recuExist(i, j-1, count))
+            if not res:
+                if (i,j) in visited:
+                    visited.remove((i,j))
+            return res
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                visited = set()
+                if recuExist(i,j, 0):
+                    return True
+        return False
---
+++ Data Structures & Algorithms/search-for-word/submission-23.py
@@ -0,0 +1,27 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def recuExist(i: int, j:int, k: int) -> bool:
+            if i > len(board) -1 or i < 0:
+                return False
+            if j > len(board[0]) -1 or j < 0:
+                return False
+            if (i, j) in visited:
+                return False
+            char = board[i][j]
+            if k == len(word)-1 and word[k] == char:
+                return True
+            if char != word[k]:
+                return False
+            visited.add((i,j))
+            
+            res = (recuExist(i+1, j, k+1) or recuExist(i, j+1, k+1) 
+            or recuExist(i-1, j, k+1) or recuExist(i, j-1, k+1))
+            
+            visited.remove((i,j))
+            return res
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                visited = set()
+                if recuExist(i,j, 0):
+                    return True
+        return False
