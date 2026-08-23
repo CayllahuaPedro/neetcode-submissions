af4f2914a7dea9302c0ee2f09d49b506ed8ec192
CayllahuaPedro <65582385+CayllahuaPedro@users.noreply.github.com>
2026-08-23 13:25:01 -0300

    Add: combination-target-sum-ii - submission-55

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-55.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        scand = sorted(candidates)
+        n = len(scand)
+        def findCombinations(start: int, total: int, curr: List[int]):
+            if total == target:
+                res.append(curr)
+                return
+            if start >= n:
+                return
+            print("start ", scand[start], total)
+            for j in range(start,n):
+                newSum = total + scand[j]
+                if newSum > target:
+                    break
+                if j > start and scand[j] == scand[j-1]:
+                    continue 
+                newCurr = [*curr, scand[j]]
+                findCombinations(j +1,newSum, newCurr)
+            print("fin, suma: ", total)
+            return 
+        findCombinations(0, 0, [])
+        return res
